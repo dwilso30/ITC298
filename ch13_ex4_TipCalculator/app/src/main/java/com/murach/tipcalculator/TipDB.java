e88ce3daeddcd4830630f6a2446453b6d86f3984
Danielle Wilson <dwilso30@seattlecentral.edu>
2017-07-03 11:55:23 -0700

    Update TipDB.java

--- ch13_ex4_TipCalculator/app/src/main/java/com/murach/tipcalculator/TipDB.java
+++ ch13_ex4_TipCalculator/app/src/main/java/com/murach/tipcalculator/TipDB.java
@@ -3,20 +3,26 @@ package com.murach.tipcalculator;
 /**
  * Created by wilso49 on 6/26/17.
  */
-import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+
+import android.content.ContentValues;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class TipDB {
 
+    // database constants
     public static final String DB_NAME = "tip.db";
     public static final int DB_VERSION = 1;
 
+    //constants
     public static final String TIP_TABLE = "tip";
 
-    public static final String TIP_ID = "_id";
+    public static final String TIP_ID = "id";
     public static final int TIP_ID_COL = 0;
 
     public static final String BILL_DATE = "bill_date";
@@ -29,57 +35,67 @@ public class TipDB {
     public static final int TIP_PERCENT_COL = 3;
 
 
+    // CREATE and DROP TABLE statements
     public static final String CREATE_TIP_TABLE =
-            "CREATE TABLE " + TIP_TABLE + " (" + TIP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "CREATE TABLE " + TIP_TABLE + " (" +
+                    TIP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     BILL_DATE + " INTEGER, " +
                     BILL_AMOUNT + " REAL, " +
                     TIP_PERCENT + "REAL);";
 
+
     public static final String DROP_TIP_TABLE =
             "DROP TABLE IF EXISTS " + TIP_TABLE;
 
+
+
     private static class DBHelper extends SQLiteOpenHelper {
 
-        public DBHelper(Context context, String name, CursorFactory factory,
-                        int version) {
+        public DBHelper(Context context, String name,
+                        CursorFactory factory, int version) {
             super(context, name, factory, version);
         }
-       @Override
-        public void onCreate(SQLiteDatabase db) {
 
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            // create tables
             db.execSQL(CREATE_TIP_TABLE);
 
-            db.execSQL("INSERT INTO tip VALUES (1, 0, 42.50, .15)");
+
+
+            db.execSQL("INSERT INTO tip VALUES (1, 0, 25.50, .15)");
             db.execSQL("INSERT INTO tip VALUES (2, 0, 25.00, .20)");
+
 
         }
 
         @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        public void onUpgrade(SQLiteDatabase db,
+                              int oldVersion, int newVersion) {
 
-            Log.d("Tip list", "Upgrading db from version " + oldVersion + " to " + newVersion);
+            Log.d("Tip list", "Upgrading db from version "
+                    + oldVersion + " to " + newVersion);
 
             db.execSQL(TipDB.DROP_TIP_TABLE);
             onCreate(db);
         }
     }
 
-    //database object and database object helper object
+    // database and database helper objects
     private SQLiteDatabase db;
     private DBHelper dbHelper;
 
-    //constructor
-    public TipDB(Context context){
+    // constructor
+    public TipDB(Context context) {
         dbHelper = new DBHelper(context, DB_NAME, null, DB_VERSION);
-
     }
 
-    //private methods
+    // private methods
     private void openReadableDB() {
         db = dbHelper.getReadableDatabase();
     }
 
-    private void openWriteableDB() {
+    private void openWritableDB() {
         db = dbHelper.getWritableDatabase();
     }
 
@@ -87,29 +103,50 @@ public class TipDB {
         if (db != null)
             db.close();
     }
-   
-    public ArrayList<Tip> getTip(int id){
+
+    // public methods
+    public ArrayList<Tip> getTips() {
         String where =
-                TIP_ID + "=?";
+                TIP_ID + "= ?";
+        int i = 1;
+        int TipID = (int) getTip(i).getId();
+        String[] whereArgs = {Integer.toString(TipID)};
+        this.openReadableDB();
+        Cursor cursor = db.query(TIP_TABLE, null,
+                null, null,
+                null, null, null);
+        ArrayList<Tip> tips = new ArrayList<Tip>();
+        while (cursor.moveToNext()) {
+            tips.add(getTipFromCursor(cursor));
+        }
+        if (cursor != null)
+            cursor.close();
+        this.closeDB();
+        return tips;
+    }
+
+
+    public Tip getTip(int id) {
+        String where = TIP_ID + "= ?";
         String[] whereArgs = {Integer.toString(id)};
 
         this.openReadableDB();
-        Cursor cursor = db.query(TIP_TABLE, null,
-                where, whereArgs, null, null, null);
-        cursor.moveToFrist();
+        Cursor cursor = db.query(TIP_TABLE,
+                null, where, whereArgs, null, null, null);
+        cursor.moveToFirst();
         Tip tip = getTipFromCursor(cursor);
-        if (cursor !=null)
+        if (cursor != null)
             cursor.close();
+        this.closeDB();
 
         return tip;
     }
 
-    private static Tip getTipFromCursor(Cursor cursor){
-        if(cursor == null || cursor.getCount()==0){
+    private static Tip getTipFromCursor(Cursor cursor) {
+        if (cursor == null || cursor.getCount() == 0) {
             return null;
-        }
-        else{
-            try{
+        } else {
+            try {
                 Tip tip = new Tip(
                         cursor.getInt(TIP_ID_COL),
                         cursor.getInt(BILL_DATE_COL),
@@ -117,8 +154,7 @@ public class TipDB {
                         cursor.getFloat(TIP_PERCENT_COL));
 
                 return tip;
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 return null;
             }
         }
